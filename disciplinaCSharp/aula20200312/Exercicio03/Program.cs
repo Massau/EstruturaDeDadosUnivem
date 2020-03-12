11652d8e34bff160ebbd43cf027f734fb371dddb
massau <joyce.santos202@etec.sp.gov.br>
2020-03-12 20:53:18 -0300

    Exercicio 03

--- disciplinaCSharp/aula20200312/Exercicio03/Program.cs
+++ disciplinaCSharp/aula20200312/Exercicio03/Program.cs
@@ -6,7 +6,17 @@ namespace Exercicio03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Tabuada do número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Deseja ver a tabuada até o número:");
+            double multiplicando = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= multiplicando; i++)
+            {
+                Console.WriteLine($"{numero} X {i} = {numero * multiplicando}");  
+            }
         }
     }
 }
