075b97d6d0d409bd6b66fa852187450ce292e57a
massau <joyce.santos202@etec.sp.gov.br>
2020-03-13 20:15:00 -0300

    Aula dia 13 mar 2020

---
+++ disciplinaCSharp/aula20200312/Exercicio05/Avaliacao.cs
@@ -0,0 +1,9 @@
+public class Avaliacao {
+    double notaOa;
+    double notaPo;
+    double media;
+
+    public void calcularMedia(){
+        media = (notaOa+notaPo/2);
+    }
+}
--- disciplinaCSharp/aula20200312/Exercicio05/Program.cs
+++ disciplinaCSharp/aula20200312/Exercicio05/Program.cs
@@ -6,7 +6,22 @@ namespace Exercicio05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Avaliacao[] avaliacao = new Avaliacao[10];
+            
+            for(int i=0; i < avaliacao.Length; i++){
+
+                avaliacao[i] = new Avaliacao();
+
+                Console.WriteLine("Digite a nota da OA");
+                avaliacao[i].notaOa = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a nota da PO");
+                avaliacao[i].notaPo = Convert.ToDouble(Console.ReadLine());
+
+                avaliacao[i].calcularMedia();
+                Console.WriteLine($"Média: {avaliacao[i].media}");
+            }
         }
     }
 }
